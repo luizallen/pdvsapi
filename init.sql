c0078482dbe7010900a539d1d3cb2ac36bc91224
Luiz Augusto Frambach <luiz_allen@hotmail.com>
2020-01-30 14:31:01 -0300

    ajustes no docker compose para utilizar o PostGis ajustes no initsql para criação da tabela de pdv

--- init.sql
+++ init.sql
@@ -1,1 +1,12 @@
-create table sometable(id int);
+CREATE EXTENSION postgis;
+CREATE EXTENSION postgis_topology;
+
+create table pdv(
+	id varchar(32),
+	tradingName varchar(100),
+	ownerName varchar(100),
+	document varchar(14),
+	coverageAreaType varchar(10),
+	coverageAreaCoordinates geometry(POINT, 4326),
+	AddressType varchar(10),
+	AddressCoordinates geometry(POINT, 4326));
